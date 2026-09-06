c0831d32acbb64ed72fc58605cd479ab32901182
PrinceLee1 <majornwa189@gmail.com>
2026-09-06 16:22:24 +0100

    daily challenge card for only users with partner

--- src/pages/GamesDashboard.tsx
+++ src/pages/GamesDashboard.tsx
@@ -482,7 +482,9 @@ export default function GamesDashboard() {
                   onClick={handleInvitePartner}
                 />
               )}
-              <DailyChallengeCard onXp={(earned) => setXp((x) => (x ?? 0) + earned)} />
+              {partnerActive && (
+                <DailyChallengeCard onXp={(earned) => setXp((x) => (x ?? 0) + earned)} />
+              )}
             </motion.div>
 
             {/* Free trial banner — every new user gets 14 days of full Plus access */}
